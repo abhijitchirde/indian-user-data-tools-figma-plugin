a954528df998381e1d0f0456bb2a2402109a75f0
Abhijit Chirde <chirde.abhi@gmail.com>
2021-12-02 16:54:14 +0530

    Update to Code and UI
    - Better handling of input message data from UI to Code
    - Corrected error handling using figma.notify in Code
    - Cleaned code for better flow

--- code.ts
+++ code.ts
@@ -5,7 +5,7 @@ figma.loadFontAsync({family: 'Roboto', style: 'Regular'});
 const userData = {
     "FirstName" : ["Ram", "Sita", "Shyam", "Bablu"],
     "LastName" : ["Shah", "Khanna", "Singh", "Yadav", "Nair", "Murti"],
-    "EmailDomain" : ["gmail", "example", "company", "bharat", "india","networks","university","cafezone", "gamers"],
+    "EmailDomain" : ["gmail", "example", "company", "bharatmail", "india","networks","university","cafezone", "gamers", "zapak", "yahoo", "rediff", "outlook", "metaverse", "miniverse", "times", "fineschool", "fisa", "yupmail", "toursnation"],
     "EmailEnd" : ["com", "in", "co.in", "net", "org", "net.in", "bharat", "biz", "co", "dev", "io"],
     "TradAddress" : [],
     "UrbAddress" : [],
@@ -13,8 +13,6 @@ const userData = {
     "State" : ["Maharashtra", "Karnataka", "Uttar Pradesh"],
     "Prof" : ["doctor", "engineer", "teacher"]
 }
-
-
 
 
 //Show UI on figma canvas
@@ -25,21 +23,31 @@ figma.showUI(__html__,{width: 420, height: 455});
 figma.ui.onmessage = msg => {
 
     //If input button is not generate this flow will work
-    if(msg.type !== "Generate"){
+    if(msg.type === "Random-details"){
+
+        //Giving notification if no layer is selected
+        if(figma.currentPage.selection.length === 0){
+            figma.notify("Please select a text layer", {timeout: 1.75, error: true});
+        }
+
         
         //for all the selected nodes
         for(const node of figma.currentPage.selection){
+
+            //Giving notification if selected layer is not text
+            if(node.type !== 'TEXT'){
+                figma.notify("Please select a text layer", {timeout: 1.75, error: true});
+            }
 
             //Checking if node is text for defining the new font (for putting text). Need to check node type as fontName is not available on Scenenode etc (error)
             setFont(node);
             
             //Calling function to put requested data on text layer
-            putTextOnLayer(node, msg.type);
+            putTextOnLayer(node, msg.inputValue);
 
         }
     
     }else if(msg.type === "Generate"){
-
         //Calling function to create a user data card and append on canvas
         generateCard();
     }  
@@ -63,34 +71,35 @@ function fullName(currentNode){
     currentNode.characters = userData["FirstName"][Math.floor(Math.random()*(userData["FirstName"].length))] + " " + userData["LastName"][Math.floor(Math.random()*(userData["LastName"].length))];
 }
 
+//Defining function for finding a number in range
+function numBetween(low, high){
+    return Math.floor(Math.random() * (high-low)) + low;
+}
+
+//Define function for generating mobile number
 function mobileNumber(currentNode){
-    let min = 7000000000;
-    let max = 9999999999;
-    let number = Math.floor(Math.random()*(max-min) + min);
+    let number = numBetween(7000000000,9999999999);         //finding a random 10 digit number for mobile
     currentNode.characters = "+91" + number;
 }
 
+//Define function for generating UID number
 function aadharNumber(currentNode){
-    let uidMin = 100000000000;
-    let uidMax = 999999999999;
-    let number = Math.floor(Math.random()*(uidMax-uidMin) + uidMin);
+    let number = numBetween(100000000000, 999999999999);    //Finding a random 12 digit number for UID
     currentNode.characters = `${number}`;
 }
 
+//Define function for generating email
 function randomEmail(currentNode){
     let fname = userData["FirstName"][Math.floor(Math.random()*(userData["FirstName"].length))];
     let lname = userData["LastName"][Math.floor(Math.random()*(userData["LastName"].length))];
     let email = userData["EmailDomain"][Math.floor(Math.random()*(userData["EmailDomain"].length))];
     let emailEnd = userData["EmailEnd"][Math.floor(Math.random()*(userData["EmailEnd"].length))];
-    let userEmail = `${fname}.${lname}@${email}.${emailEnd}`;
-    currentNode.characters = userEmail.toLowerCase();
+    currentNode.characters = `${fname}.${lname}@${email}.${emailEnd}`.toLowerCase();
 }
 
-function pinCode(currentNode){
-    let pinFirst = Math.floor(Math.random()*9);
-    let pinRemainingMin = 10000;
-    let pinRemainingMax = 99999;
-    let pinRemaining = Math.floor(Math.random()*(pinRemainingMax - pinRemainingMin)+pinRemainingMin);
+function randomPINCode(currentNode){
+    let pinFirst = Math.floor(Math.random()*8 + 1);
+    let pinRemaining = numBetween(10000,99999);             //Finding a random 5 digit number for remaining part of PIN
     currentNode.characters = `${pinFirst}${pinRemaining}`;
 }
 
@@ -112,9 +121,10 @@ function putTextOnLayer(currentNode, input){
             randomEmail(currentNode);
         }else if(input === "PIN"){
             //Generate PIN code using function
-            pinCode(currentNode);
+            randomPINCode(currentNode);
         }
-        //Otherwise for single piece of data, we can directly call the dataspace array's random element with help of input messsage as string input. That's why we used msg inputs and array names same, and used arrays as a large object of array
+        //Otherwise for single piece of data from arry, we can directly call the dataspace array's random element with help of input messsage as string input. That's why we used msg inputs and array names same, and used arrays as a large object of array
+        //This can work for firstname, lastname, city, state, profession
         else{
             separateItem(currentNode, input);
         }
